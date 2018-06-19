7fc181ab1b046dcb27f81fbf807c70c2ab1cebd8
Raphael Jolly <rjolly@users.sourceforge.net>
2018-06-19 14:34:48 +0200

    empty

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 mkdir("build");
 mkdir("build/classes");
 
-javac("src/application", "build/classes");
+javac("src", "build/classes");
+copy("res", "build/classes");
 
 mkdir("dist");
 jar("dist/jgraphpad.jar", "build/classes", ".*", "manifest.mf");
